aa212ad66acfb62800e6ee0f8db2823d8a9058f6
root <root@DESKTOP-B4NLVDC.localdomain>
2022-03-04 07:29:56 +0700

    add sort logic

--- openmpi.cpp
+++ openmpi.cpp
@@ -113,6 +113,60 @@ void decompose_matrix(Matrix *m)
     }
 }
 
+
+/*
+ * Procedure merge_array
+ *
+ * Merges two subarrays of n with n[left..mid] and n[mid+1..right]
+ * to n itself, with n now ordered ascendingly
+ * */
+void merge_array(int *n, int left, int mid, int right) {
+	int n_left = mid - left + 1;
+	int n_right = right - mid;
+	int iter_left = 0, iter_right = 0, iter_merged = left;
+	int arr_left[n_left], arr_right[n_right];
+
+	for (int i = 0; i < n_left; i++) {
+		arr_left[i] = n[i + left];
+	}
+
+	for (int i = 0; i < n_right; i++) {
+		arr_right[i] = n[i + mid + 1];
+	}
+
+	while (iter_left < n_left && iter_right < n_right) {
+		if (arr_left[iter_left] <= arr_right[iter_right]) {
+			n[iter_merged] = arr_left[iter_left++];
+		} else {
+			n[iter_merged] = arr_right[iter_right++];
+		}
+		iter_merged++;
+	}
+
+	while (iter_left < n_left)  {
+		n[iter_merged++] = arr_left[iter_left++];
+	}
+	while (iter_right < n_right) {
+		n[iter_merged++] = arr_right[iter_right++];
+	} 
+}
+
+/* 
+ * Procedure merge_sort
+ *
+ * Sorts array n with merge sort algorithm
+ * */
+void merge_sort(int *n, int left, int right) {
+	if (left < right) {
+		int mid = left + (right - left) / 2;
+
+		merge_sort(n, left, mid);
+		merge_sort(n, mid + 1, right);
+
+		merge_array(n, left, mid, right);
+	}	
+}
+
 int main(int argc, char *argv[])
 {
     int size;
@@ -122,6 +176,8 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int* myArray;
+    int sizeMyArray=0;
 
     if (rank == 0)
     {
@@ -190,6 +246,58 @@ int main(int argc, char *argv[])
             // Do kerjaan slave
             // KernelMatrix buat slave ini itu kernelMatrix[0] sampai kernelMatrix[numTargets-1] atau sejumlah numTargets
         }
+
+        //TODO initate the value of myArray and sizeMyArray with the result of convolution. Array with one element i think.
+    }
+
+    /* Sorting Logic Starts From Here!1!1!1! */
+    
+    //variable to help
+    int divisor = 2;
+    int divisor_difference = 1;
+
+    //start process
+    while (divisor <= size){
+        if (rank % divisor == 0){
+            int rankPartner = rank + divisor_difference
+            if (rankPartner < size){    //check condition is the partner exist
+                //receive size of array
+                int sizeOfRecvArray;
+                MPI_Recv(&sizeOfRecvArray, 1, MPI_INT, rankPartner, 1, MPI_COMM_WORLD, 0);  //tag 1 only for differentiate this message to get size of array
+                
+                //receive array
+                int arrayOfInt[sizeOfRecvArray];
+                MPI_Recv(arrayOfInt, sizeOfRecvArray, MPI_INT, rankPartner, 2, MPI_COMM_WORLD, 0); //tag 2 only for differentiate this message to get the array
+
+                //unite myArray with received array
+                sizeMyArray = sizeMyArray + sizeOfRecvArray
+                int* tempArray;
+                tempArray = (int *)malloc((sizeMyArray)*sizeof(int));
+                for (int i=0; i<sizeMyArray; i++){
+                    tempArray[i] = myArray[i];
+                }
+                for (int i=0; i<sizeOfRecvArray; i++){
+                    tempArray[i+sizeMyArray] = arrayOfInt[i];
+                }
+                free(myArray);
+                myArray = tempArray;
+                tempArray = NULL;
+
+                //sort array
+                merge_sort(myArray, 0, sizeMyArray-1);
+            }
+        }else{
+            int rankPartner = rank - divisor_difference;
+
+            //send size of array
+            MPI_Send(&sizeMyArray, 1, MPI_INT, rankPartner, 1, MPI_COMM_WORLD);
+
+            //send the array
+            MPI_Send(myArray, sizeMyArray, MPI_INT, rankPartner, 2, MPI_COMM_WORLD);
+        }
+
+        divisor *= 2
+        divisor_difference *= 2
     }
 
     MPI_Finalize();
